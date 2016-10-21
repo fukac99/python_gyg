5be12b1f5e0d519b0c63ef9ab8da7beec8899ac7
Lukas <toma.lukas@gmail.com>
2016-10-21 11:17:16 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python_gyg',
+      version='0.1',
+      description='Python SDK for GetYourGuide API',
+      url='http://github.com//funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
